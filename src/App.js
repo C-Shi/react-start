2605bcc48355068be69b0a0f91a78c96b17895b7
Cheng Shi <Kyle@Chengs-MacBook-Pro.local>
2018-04-25 22:23:44 -0600

    dynamic css and Radium

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './Person/Person.css'
-import Person from './Person/Person'
+import './Person/Person.css';
+import Radium, {StyleRoot} from 'radium'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    person: [
-      {name: "Max", age:28},
-      {name: "Kyle", age:25}
+    person:[
+      {id:'1', name:'Kyle', age: 22},
+      {id:'2', name: 'Paul', age: 23},
+      {id:'3', name:'Selina', age:21}
     ],
+
     otherState: "some other value",
     showPerson: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      person:[
-        {name: 'Max', age:28},
-        {name: event.target.value, age: 29}
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.person.findIndex(p => {
+      return p.id === id;
     })
-  }
+
+    const person = {
+      ...this.state.person[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.person];
+    persons[personIndex] = person;
+  
+    this.setState({
+      person:persons
+    })}
 
   toggleHandler = () =>{
     const doesShow = this.state.showPerson;
@@ -45,38 +57,66 @@ class App extends Component {
       backgrondColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      padding: '8px'
+      padding: '8px',
+      // radium support all sudo selector but need to be a string state
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color:'color'
+      }
     };
+
+    
+    style['hover'] = {
+      backgrondColor: 'salmon',
+      color: 'black'
+    }
 
     let person = null;
 
     if (this.state.showPerson) {
-      person = (
-        <div>
-          {/* map method passing two argument, the array element and the index */}
-          {this.state.person.map((person, personIndex) => {
-            return <Person name={person.name} age={person.age} click={ () => this.deletePersonHandler(personIndex)}/>
-          })}     
-        </div>
-      )
+      if ( this.state.showPerson ) {
+        person = (
+          <div>
+            {this.state.person.map((person, index) => {
+              return <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                change={(event) => this.nameChangedHandler(event, person.id)} />
+            })}
+          </div>
+        );
+      }
+    }
+
+    // manipulate CSS style 
+    const classes = []
+    if (this.state.person.length <= 2){
+      classes.push('red');
+    };
+    if (this.state.person.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi This is the react app</h1>
-        <p>I will be strat writing my first react code in this file</p>
-        {/* only method define under 'state' can be accessed by 'this' */}
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi This is the react app</h1>
+          <p className={classes.join(' ')}>I will be strat writing my first react code in this file</p>
+          {/* only method define under 'state' can be accessed by 'this' */}
 
-        <button onClick={this.toggleHandler} style={style}>
-          Toggle Name
-        </button>
-        {person}
-      </div>
-
+          <button onClick={this.toggleHandler} style={style}>
+            Toggle Name
+          </button>
+          {person}
+        </div>
+      </StyleRoot>
     )
     // );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, "Does this work?"));
   }
 }
 
-export default App;
+// a higher order component, radium allows us to use sudo selector and media query CSS 
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Radium from 'radium';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm a {props.name}! I am {props.age}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change}/>
+            <input type="text" onChange={props.change} value={props.name}/>
         </div>
     )
 }
 
 // cannot use setState even you assign a 'state', because it has no Component
 
-export default person;
+export default Radium(person);
